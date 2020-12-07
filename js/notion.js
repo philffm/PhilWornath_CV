bf66ba8b6c9596c6044b2cff4ef9f9c6c0693dba
Phil Wornath <philipp@wornath.de>
2020-12-07 02:35:07 +0100

    Content and stability updates

--- js/notion.js
+++ js/notion.js
@@ -10,40 +10,57 @@ const ytEmbed = (sourceID) => {
 }
 
 function replaceStuff(){
+    waitingFor(20,20,'code',replaceCode);
 
-var elSources = document.querySelectorAll('.source');
-var elProperties = document.querySelector('.properties').firstChild.querySelectorAll('tr');
-var elCode = document.querySelectorAll('code');
-
-    for(i=0;i < elProperties.length;i++){
-        let propContent = elProperties[i].lastChild.innerHTML;
-        if (propContent == ''){
-            elProperties[i].innerHTML="";
-        }
-        
-    }
     
-    for(i=0;i < elSources.length;i++){
-        let sourceID = elSources[i].firstChild.attributes.href.value.split("=")[1];
-        let includesYT = elSources[i].firstChild.attributes.href.value.split("=")[0].includes('yout'); 
-        if (sourceID && includesYT == true){
-            includesYT = elSources[i].firstChild.innerHTML = ytEmbed(sourceID);
-        }
-        
-    }
 
-    for(i=0;i < elCode.length;i++){
-        let code = elCode[i].innerText;
-        let includeCode = code.includes('@include'); 
-        let codeClean = code.replaceAll('@include','');
-        if (includeCode){
-            elCode[i].parentElement.outerHTML=codeClean;
+let elProperties = document.querySelector('.properties').firstChild.querySelectorAll('tr');
+let elSources = document.querySelectorAll('.source');
+
+    if(elProperties){
+        for(i=0;i < elProperties.length;i++){
+            let propContent = elProperties[i].lastChild.innerHTML;
+            if (propContent == '' ){
+                elProperties[i].innerHTML="";
+            }else{}
+            
         }
-        
-    }
+
+    }else{}
+    
+    if(elSources){
+       for(i=0;i < elSources.length;i++){
+           let sourceID = elSources[i].firstChild.attributes.href.value.split("=")[1];
+           let includesYT = elSources[i].firstChild.attributes.href.value.split("=")[0].includes('yout'); 
+           if (sourceID && includesYT == true){
+                includesYT = elSources[i].firstChild.innerHTML = ytEmbed(sourceID);
+            }else{}
+            
+        }
+    }else{}
+
+
+
+   
 
 
 }
+
+function replaceCode(){
+    let elCode = document.querySelectorAll('code');
+    if(elCode){
+        for(i=0;i < elCode.length;i++){
+            let code = elCode[i].innerText;
+            let includeCode = code.includes('@include'); 
+            let codeClean = code.replaceAll('@include','');
+            if (includeCode){
+                elCode[i].parentElement.outerHTML=codeClean;
+            }else{}
+            
+        }
+    }else{}
+}
+
 
 
 
